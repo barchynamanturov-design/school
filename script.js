7c8584aa0857931fa3a9cd7c0896ec79b2d6a456
Barchyn <barchynamanturov@gmail.com>
2025-10-24 18:39:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let car=prompt(`пеши оценку?`)
+switch (car) {
+    case `2`:
+        alert(`плохо😒`)
+        break;
+    case `3`:
+        alert (`пайдёт😐`)
+        break;
+    case`4`:
+        alert(`хорошо😊`)
+        break;
+    case `5`:
+        alert(`очень хорошо😁`)
+        break;
+}
